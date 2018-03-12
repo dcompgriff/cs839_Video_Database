746d7e452e169995cbcc575dcc6011f0509da332
Daniel Griffin <dcompgriff@gmail.com>
2018-03-12 07:22:56 -0500

    Added scraping scripts for IMDB website, and MovieNumbers website.

---
+++ scrapeIMDB.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+
+
+'''
+Website: http://www.imdb.com/list/ls032600534/?sort=list_order,asc&st_dt=&mode=detail&page=1
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
